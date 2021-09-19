cbc1febcbe35f277a0b52e0d4faceab28cca978b
Yacine Merzouk <yacine@merzouk.ca>
2021-09-18 15:30:51 -1000

    Updated for Meteor 2.3.6

--- package.js
+++ package.js
@@ -9,8 +9,8 @@ Npm.depends({
   'body-parser': '1.19.0'
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@2.2');
+Package.onUse(function (api) {
+  api.versionsFrom('METEOR@2.3.6');
   // meteor dependencies
   api.use('underscore');
   api.use('webapp', 'server');
@@ -25,52 +25,52 @@ Package.on_use(function (api) {
   api.use('meteor');
 
   // main namespace and utils
-  api.use('meteorhubdotnet:firestorm-iron-core@1.0.4');
-  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.4');
+  api.use('meteorhubdotnet:firestorm-iron-core@1.0.5');
+  api.imply('meteorhubdotnet:firestorm-iron-core@1.0.5');
 
   // ui layout
-  api.use('meteorhubdotnet:firestorm-iron-layout@1.0.4');
+  api.use('meteorhubdotnet:firestorm-iron-layout@1.0.5');
 
   // connect like middleware stack for client/server
-  api.use('meteorhubdotnet:firestorm-iron-middleware-stack@1.0.4');
+  api.use('meteorhubdotnet:firestorm-iron-middleware-stack@1.0.5');
 
   // client and server side url utilities and compiling
-  api.use('meteorhubdotnet:firestorm-iron-url@1.0.4');
+  api.use('meteorhubdotnet:firestorm-iron-url@1.0.5');
 
   // for reactive urls and pushState in the browser
-  api.use('meteorhubdotnet:firestorm-iron-location@1.0.4');
+  api.use('meteorhubdotnet:firestorm-iron-location@1.0.5');
 
   // for RouteController which inherits from this
-  api.use('meteorhubdotnet:firestorm-iron-controller@1.0.4');
+  api.use('meteorhubdotnet:firestorm-iron-controller@1.0.5');
 
-  api.add_files('lib/current_options.js');
-  api.add_files('lib/http_methods.js');
-  api.add_files('lib/route_controller.js');
-  api.add_files('lib/route_controller_server.js', 'server');
-  api.add_files('lib/route_controller_client.js', 'client');
-  api.add_files('lib/route.js');
-  api.add_files('lib/router.js');
-  api.add_files('lib/hooks.js');
-  api.add_files('lib/helpers.js');
-  api.add_files('lib/router_client.js', 'client');
-  api.add_files('lib/body_parser_server.js', 'server');
-  api.add_files('lib/router_server.js', 'server');
-  api.add_files('lib/plugins.js');
-  api.add_files('lib/global_router.js');
-  api.add_files('lib/templates.html');
+  api.addFiles('lib/current_options.js');
+  api.addFiles('lib/http_methods.js');
+  api.addFiles('lib/route_controller.js');
+  api.addFiles('lib/route_controller_server.js', 'server');
+  api.addFiles('lib/route_controller_client.js', 'client');
+  api.addFiles('lib/route.js');
+  api.addFiles('lib/router.js');
+  api.addFiles('lib/hooks.js');
+  api.addFiles('lib/helpers.js');
+  api.addFiles('lib/router_client.js', 'client');
+  api.addFiles('lib/body_parser_server.js', 'server');
+  api.addFiles('lib/router_server.js', 'server');
+  api.addFiles('lib/plugins.js');
+  api.addFiles('lib/global_router.js');
+  api.addFiles('lib/templates.html');
 
   // symbol exports
   api.export('Router');
   api.export('RouteController');
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use('meteorhubdotnet:firestorm-iron-router');
   api.use('tinytest');
   api.use('test-helpers');
 
-  api.add_files('test/helpers.js');
-  api.add_files('test/route_test.js');
-  api.add_files('test/router_test.js');
-  api.add_files('test/route_controller_test.js');
+  api.addFiles('test/helpers.js');
+  api.addFiles('test/route_test.js');
+  api.addFiles('test/router_test.js');
+  api.addFiles('test/route_controller_test.js');
 });
